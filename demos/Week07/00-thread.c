fee61c9de9043bdde553445d6251502db68fffb4
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-04-11 17:46:28 +0700

    rmsbase Week07

--- demos/Week07/00-thread.c
+++ demos/Week07/00-thread.c
@@ -13,54 +13,42 @@
 #include <unistd.h>
 #include "99-myutils.h"
 
-#define  LOOP0     5
-#define  LOOP1   500
-#define  LOOP2  1000
-#define  LOOP3 20000
+#define  LOOP0      5
+#define  LOOP1 500900
+#define  LOOP2 500000
 
 volatile int loop = LOOP0;
 int          share;
 
 void* thread1 (void* a) {
-   int ii, jj, kk;
+   int ii, jj=0;
    printf("I am a thread no 1\n");
    sleep(1);
    share = 1000;
    while (loop > 0) {
-      for (ii=0;ii<LOOP1;ii++) {
-         for (jj=0;jj<LOOP2;jj++) {
-              ;
-         }
-      }
+      for (ii=0;ii<LOOP1;ii++) 
+         jj++;
       share++;
    }
 }
 
 void* thread2 (void* a) {
-   int ii, jj, kk;
+   int ii, jj=0;
    printf("I am a thread no 2\n");
    sleep(1);
    share = 2000;
    while (loop > 0) {
-      for (ii=0;ii<LOOP1;ii++) {
-         for (jj=0;jj<LOOP2;jj++) {
-              ;
-         }
-      }
+      for (ii=0;ii<LOOP2;ii++) 
+         jj++;
       share--;
    }
 }
 
 void* thread3 (void* a) {
-   int ii, jj, kk;
    printf("I am a thread no 3\n");
    sleep(1);
    while (loop-- > 0) {
-      for (ii=0;ii<LOOP3;ii++) {
-         for (jj=0;jj<LOOP3;jj++) {
-              ;
-         }
-      }
+      sleep(2);
       printf("SHARE = %4.4d\n", share);
    }
 }
