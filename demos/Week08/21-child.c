922184fcd460f695552905e80fa9de017babfc26
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-04-23 20:48:10 +0700

    rmsbase Week08

--- demos/Week08/21-child.c
+++ demos/Week08/21-child.c
@@ -7,13 +7,13 @@
  * purpose, even commercially).  This program is distributed in the hope that 
  * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * REV02 Wed Apr 18 22:03:23 WIB 2018
+ * REV03 Mon Apr 23 17:01:28 WIB 2018
  * REV00 Wed Apr 18 19:50:01 WIB 2018
  * START Xxx Xxx XX XX:XX:XX WIB 2015
  */
 
 
-#define ITERS 5
+#define ITERS 2
 #define SEM_NAME "/semaphore-demo-rms46"
 /* ATTN:
    "Dead semaphores" are lingering in folder "/dev/shm/".
