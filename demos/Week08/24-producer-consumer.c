9e0cab5630a6e137fdfc4541ce89b2ad5d425e4f
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-05-01 20:36:32 +0700

    pamulang1 Week08

---
+++ demos/Week08/24-producer-consumer.c
@@ -0,0 +1,102 @@
+﻿/*
+ Copyright 2018 Rahmat M. Samik-Ibrahim
+ You are free to SHARE (copy and 
+ redistribute the material in any medium
+ or format) and to ADAPT (remix, 
+ transform, and build upon the material 
+ for any purpose, even commercially).
+ This program is distributed in the hope 
+ that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied 
+ warranty of MERCHANTABILITY or FITNESS 
+ FOR A PARTICULAR PURPOSE.
+
+ * REV01 Tue May  1 19:56:35 DST 2018
+ * START Wed Apr 18 19:50:01 WIB 2018
+ */
+
+// DO NOT USE THE SAME SEMAPHORE NAME!!!!
+// Replace "demo" with your own SSO name.
+#define SEM_COUNT1      "/count-1-demo"
+#define SEM_COUNT2      "/count-2-demo"
+#define SEM_MUTEX       "/mutex-demo"
+#define SEM_SYNC        "/sync-demo"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// Shared Memory: R/W with no name.
+#define PROT    (PROT_READ | PROT_WRITE)
+#define VISIBLE (MAP_ANON  | MAP_SHARED)
+
+#define LOOP    2
+#define BUFSIZE 1
+
+sem_t*  ctr_prod;
+sem_t*  ctr_cons;
+sem_t*  mutex;
+sem_t*  ssync;
+int*    product;
+
+// WARNING: NO ERROR CHECK! ////////////
+void init(void) {
+   product  = mmap(NULL, sizeof(int), 
+                   PROT, VISIBLE, 0, 0);
+   *product = 0;
+   ctr_prod = sem_open(SEM_COUNT1, 
+              O_CREAT, 0600, BUFSIZE);
+   ctr_cons = sem_open(SEM_COUNT2, 
+              O_CREAT, 0600, 0);
+   mutex    = sem_open(SEM_MUTEX, 
+              O_CREAT, 0600, 1);
+   ssync    = sem_open(SEM_SYNC, 
+              O_CREAT, 0600, 0);
+}
+
+void producer (void) {
+   sem_wait(ssync);
+   printf("PRODUCER PID[%d]\n",getpid());
+   for (int loop = 0; loop < LOOP; loop++) {
+      sem_wait(ctr_prod);
+      sem_wait(mutex);
+      printf("PRODUCER %d\n",++(*product));
+      sem_post(mutex);
+      sem_post(ctr_cons);
+   }
+   wait(NULL);
+}
+
+void consumer (void) {
+   printf("CONSUMER PID[%d]\n",getpid());
+   sem_post(ssync);
+   for (int loop = 0; loop < LOOP; loop++) {
+      sem_wait(ctr_cons);
+      sem_wait(mutex);
+      printf("CONSUMER %d\n", *product);
+      sem_post(mutex);
+      sem_post(ctr_prod);
+   }
+}
+
+// WARNING: NO ERROR CHECK! ////////////
+void main(void) {
+   printf("STARTING PID[%d]\n", getpid());
+   init();
+   if (fork()) producer (); //Parent
+   else        consumer (); //Child
+   sem_unlink(SEM_COUNT1);
+   sem_unlink(SEM_COUNT2);
+   sem_unlink(SEM_SYNC);
+   sem_unlink(SEM_MUTEX);
+   printf("STOP PID[%d]\n", getpid());
+}
+
+//       1         2         3         4
+// 4567890123456789012345678901234567890
+
