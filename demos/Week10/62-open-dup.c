046c1ad528b77a71b93b3cd1b16d0e719e956986
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-05-18 14:00:06 +0700

    rmsbase Week10

---
+++ demos/Week10/62-open-dup.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2018 Rahmat M. Samik-Ibrahim
+ * http://rahmatm.samik-ibrahim.vlsm.org/
+ * This program is free script/software. This program is distributed in the 
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV00 Fri May 18 13:36:57 WIB 2018
+ * START Fri May 18 13:36:57 WIB 2018
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#define FILE "62-open-dup.txt"
+
+void main(void) {
+}
+
